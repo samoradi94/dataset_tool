7bbb86e612697a3826db940d22f960ae634d6f9e
samoradi94 <samoradi94@gmail.com>
2018-06-21 01:35:55 +0430

    static folder name

--- src/dataset_tools/main.java
+++ src/dataset_tools/main.java
@@ -15,35 +15,31 @@ public class main {
 		// TODO Auto-generated method stub
 
 		Instances reduced ;
+		int percent = 90;
+		int seedNumber = 1;
+		String dataset_name = "iris";
 		// data
-		
+
 		ArffLoader arffLoader = new ArffLoader();
-		arffLoader.setSource(new File("/home/saeideh/iris.arff"));
+		arffLoader.setSource(new File("/home/saeideh/"+ dataset_name+ ".arff"));
 		Instances data = arffLoader.getDataSet();
 
-//		String Fliteroptions="-S 1";
 		Resample sampler = new Resample();
 		sampler.setInputFormat(data);
-		sampler.setSampleSizePercent(90);
-		sampler.setRandomSeed(2);
+		sampler.setSampleSizePercent(percent);
+		sampler.setRandomSeed(seedNumber);
 		sampler.setNoReplacement(true);
-//		reduced = Resample.useFilter(data, sampler);
 		reduced =  Filter.useFilter(data, sampler);
-		
-//		resampleFilter.setRandomSeed(1);
-//		resampleFilter.setBiasToUniformClass(1);
-//		resampleFilter.setInputFormat(data);
-//		Instances reduced = Filter.useFilter(data, resampleFilter);
 		for (int i = 0; i < reduced.numInstances(); i++) {
 			System.out.println(reduced.instance(i));
 		}
-		
+
 
 		ArffSaver saver = new ArffSaver();
-		 saver.setInstances(reduced);
-		 saver.setFile(new File("./data/test.arff"));
-		 saver.setDestination(new File("./data/test.arff"));   // **not** necessary in 3.5.4 and later
-		 saver.writeBatch();
+		saver.setInstances(reduced);
+//		saver.setFile(new File("./data/test.arff"));
+		saver.setFile(new File("data/"+ dataset_name + "- " + seedNumber + "-" + percent + ".arff"));
+		saver.writeBatch();
 
 
 	}
